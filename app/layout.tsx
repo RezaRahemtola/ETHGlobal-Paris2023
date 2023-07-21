d122f28702ab61ed661915f6534e52545e329212
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-07-21 21:21:22 +0200

    style: prettier fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,22 +9,22 @@ import { Providers } from "./providers";
 import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html suppressHydrationWarning lang="en">
-      {/*
+	return (
+		<html suppressHydrationWarning lang="en">
+			{/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+			<head />
 
-      <body className="dark:bg-black">
-        <Providers>
-          <Header />
-          {children}
-          <Footer />
-          <ScrollToTop />
-        </Providers>
-      </body>
-    </html>
-  );
+			<body className="dark:bg-black">
+				<Providers>
+					<Header />
+					{children}
+					<Footer />
+					<ScrollToTop />
+				</Providers>
+			</body>
+		</html>
+	);
 }
