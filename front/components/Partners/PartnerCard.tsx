992a7267b7d03119ee71d1da6a1f7c0547e939a3
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-07-22 15:20:29 +0200

    feat(front): App logo

--- front/components/Partners/PartnerCard.tsx
+++ front/components/Partners/PartnerCard.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import Image from "next/image";
 import { Partner } from "@/types/partners";
+import Link from "next/link";
 
 type PartnerCardProps = {
 	partner: Partner;
 };
 const PartnerCard = ({ partner }: PartnerCardProps) => {
-	const { name, image } = partner;
+	const { name, description, link, image } = partner;
 	return (
-		<div className="relative overflow-hidden rounded-md bg-white shadow-one dark:bg-dark">
-			<div className="relative block h-[220px] w-[220px] mx-auto">
-				<Image src={image} alt={`partner-${name.toLowerCase()}-logo`} fill className="pt-3 " />
+		<Link href={link} target="_blank">
+			<div className="relative overflow-hidden rounded-md bg-white shadow-one dark:bg-dark">
+				<div className="relative block h-[220px] w-[220px] mx-auto">
+					<Image src={image} alt={`partner-${name.toLowerCase()}-logo`} fill className="pt-3 " />
+				</div>
+				<div className="p-6 sm:p-8 md:py-8 md:px-6 lg:p-8 xl:py-8 xl:px-5 2xl:p-8">
+					<h3 className="border-t border-body-color border-opacity-10 pt-6 text-center dark:text-white mb-1 block text-xl font-bold text-black sm:text-2xl">
+						{name}
+					</h3>
+					<br />
+					<p className="text-center">{description}</p>
+				</div>
 			</div>
-			<div className="p-6 sm:p-8 md:py-8 md:px-6 lg:p-8 xl:py-8 xl:px-5 2xl:p-8">
-				<h3 className="border-t border-body-color border-opacity-10 pt-6 text-center dark:text-white mb-1 block text-xl font-bold text-black sm:text-2xl">
-					{name}
-				</h3>
-			</div>
-		</div>
+		</Link>
 	);
 };
 
--- front/config/partners.ts
+++ front/config/partners.ts
@@ -5,12 +5,14 @@ const partners: Partner[] = [
 		id: 1,
 		name: "DFNS",
 		description: "TODO description",
+		link: "https://www.dfns.co/",
 		image: "/images/partners/dfns.png",
 	},
 	{
 		id: 2,
 		name: "Semaphore",
 		description: "TODO description",
+		link: "https://github.com/semaphore-protocol",
 		image: "/images/partners/semaphore.svg",
 	},
 ];
