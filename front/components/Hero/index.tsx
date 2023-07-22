c0d1c196551b6e32d1b85dcb9b90cf82101c9063
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-07-22 12:01:30 +0200

    feat(front): Page name started

--- front/components/Hero/index.tsx
+++ front/components/Hero/index.tsx
@@ -23,14 +23,16 @@ const Hero = () => {
 									<Link
 										href="https://nextjstemplates.com/templates/saas-starter-startup"
 										className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+										target="_blank"
 									>
 										🔥 Get Pro
 									</Link>
 									<Link
-										href="https://github.com/NextJSTemplates/startup-nextjs"
+										href="https://github.com/RezaRahemtola/ETHGlobal-Paris2023"
+										target="_blank"
 										className="rounded-md bg-black/20 py-4 px-8 text-base font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
 									>
-										Star on GitHub
+										⭐ Star on GitHub
 									</Link>
 								</div>
 							</div>
