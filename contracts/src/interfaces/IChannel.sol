a59d126e03ecc61c7dbd9f0a7dc8ae96abdd70fd
Isma <71719097+Doozers@users.noreply.github.com>
2023-07-22 15:47:25 +0200

    feat: non working contracts
    Signed-off-by: Isma <71719097+Doozers@users.noreply.github.com>

--- contracts/src/interfaces/IChannel.sol
+++ contracts/src/interfaces/IChannel.sol
@@ -1,11 +1,33 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.21;
+pragma solidity 0.8.4;
 
 interface IChannel {
 
-    event messageSent(uint256 id, bytes32 contentCID);
+    event MessageSent(uint256 indexed id, bytes32 contentCID);
 
-    function joinGroup(bytes calldata request) external;
+    event NewMember(address indexed member);
 
-    function message(bytes calldata entry) external;
+    event MemeberCredentials(address indexed member, uint256 indexed identityCommitment);
+
+    /**
+     * @dev Gets the name of the channel.
+     * @return The name of the channel.
+     */
+    function name() external view returns (bytes32);
+
+    /**
+    * @dev The type of channel.
+    * @return 3 bits 0x123. 1 = is it private chat, 2 = semaphore anonymous chat, 3 = joinable
+    */
+    function getType() external view returns (bytes1);
+
+    /**
+     * @dev Gets the CID of the joining rules of the channel.
+     * @return The CID of the joining rules of the channel.
+     */
+    function getJoiningRules() external view returns (bytes32);
+
+    function join(bytes calldata entry) external;
+
+    function sendMessage(bytes calldata entry) external;
 }
