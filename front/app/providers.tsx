a47c09a64ce188da454861f85802b2baf4ed7484
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-07-22 16:19:16 +0200

    feat(front): Favicon and title

--- front/app/providers.tsx
+++ front/app/providers.tsx
@@ -2,8 +2,9 @@
 
 import { ThemeProvider } from "next-themes";
 import { MetaMaskContextProvider } from "@/utils/useMetaMask";
+import { ReactNode } from "react";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children }: { children: ReactNode }) {
 	return (
 		<ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
 			<MetaMaskContextProvider>{children}</MetaMaskContextProvider>
