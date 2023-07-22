61b03ccd22ce3204496947c839881761d56069d7
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-07-22 23:29:40 +0200

    feat: Rooms menu navigation

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -3,7 +3,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
-import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 import { Providers } from "./providers";
 import { ReactNode } from "react";
