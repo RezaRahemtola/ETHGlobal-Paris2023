490dd44102d90b5843ef3b393610f21cd311f0ed
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-07-23 00:26:35 +0200

    feat: Get started link

--- front/components/Hero.tsx
+++ front/components/Hero.tsx
@@ -18,9 +18,8 @@ const Hero = () => (
 						</p>
 						<div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
 							<Link
-								href="https://nextjstemplates.com/templates/saas-starter-startup"
+								href="/rooms"
 								className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
-								target="_blank"
 							>
 								🔥 Get Started
 							</Link>
