1a3f5d2096ff207a901b65bb0d613f6a9b734e48
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-07-22 12:31:26 +0200

    feat(front): Landing page refactored

--- front/components/Hero/index.tsx
+++ front/components/Hero/index.tsx
@@ -12,12 +12,10 @@ const Hero = () => {
 						<div className="w-full px-4">
 							<div className="wow fadeInUp mx-auto max-w-[800px] text-center" data-wow-delay=".2s">
 								<h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-									Free and Open-Source Next.js Template for Startup & SaaS
+									TODO messaging app
 								</h1>
 								<p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-									Startup is free Next.js template for startups and SaaS business websites comes with all the essential
-									pages, components, and sections you need to launch a complete business website, built-with Next 13.x
-									and Tailwind CSS.
+									TODO description
 								</p>
 								<div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
 									<Link
@@ -25,7 +23,7 @@ const Hero = () => {
 										className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
 										target="_blank"
 									>
-										🔥 Get Pro
+										🔥 Get Started
 									</Link>
 									<Link
 										href="https://github.com/RezaRahemtola/ETHGlobal-Paris2023"
---
+++ front/config/partners.ts
@@ -0,0 +1,17 @@
+import { Partner } from "@/types/partners";
+
+const partners: Partner[] = [
+	{
+		id: 1,
+		name: "DFNS",
+		description: "TODO description",
+		image: "/images/partners/dfns.png",
+	},
+	{
+		id: 2,
+		name: "Semaphore",
+		description: "TODO description",
+		image: "/images/partners/semaphore.svg",
+	},
+];
+export default partners;
