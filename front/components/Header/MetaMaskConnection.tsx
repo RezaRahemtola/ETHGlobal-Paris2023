48e699b777ca97e2771eb5637a5bed5ea1971dc9
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-07-22 15:23:44 +0200

    fix(front/metamask): Useless dependencies and style

--- front/components/Header/MetaMaskConnection.tsx
+++ front/components/Header/MetaMaskConnection.tsx
@@ -4,14 +4,12 @@ import Link from "next/link";
 
 export default function MetaMaskConnection() {
 	const { wallet, hasProvider, isConnecting, connectMetaMask } = useMetaMask();
-	const buttonStyle = "ease-in-up hidden rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp md:block md:px-9 lg:px-6 xl:px-9"
+	const buttonStyle =
+		"ease-in-up hidden rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp md:block md:px-9 lg:px-6 xl:px-9";
 
 	if (!hasProvider) {
 		return (
-			<Link
-				href="https://metamask.io"
-				className={buttonStyle}
-			>
+			<Link href="https://metamask.io" className={buttonStyle}>
 				Install Metamask
 			</Link>
 		);
@@ -19,21 +17,14 @@ export default function MetaMaskConnection() {
 
 	if (window.ethereum?.isMetaMask && wallet.accounts.length < 1) {
 		return (
-			<button
-				disabled={isConnecting}
-				onClick={connectMetaMask}
-				className={buttonStyle}
-			>
+			<button disabled={isConnecting} onClick={connectMetaMask} className={buttonStyle}>
 				Connect MetaMask
 			</button>
 		);
 	}
 
 	return (
-		<button
-			disabled
-			className={buttonStyle}
-		>
+		<button disabled className={buttonStyle}>
 			{wallet.accounts[0].substring(0, 8)}...
 		</button>
 	);
